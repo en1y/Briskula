a86cf1f8585140c45c112846a324f3ead929bf0b
en1y <marsentiy.cooo@gmail.com>
2025-05-05 14:56:43 +0200

    implemented Card.isTrumpCard

--- BriskulaLogic/src/main/java/cards/Card.java
+++ BriskulaLogic/src/main/java/cards/Card.java
@@ -4,4 +4,7 @@ public record Card (
     CardType type,
     CardValue value
 ){
+    public boolean isTrumpCard(CardType trumpCardType) {
+        return this.type().equals(trumpCardType);
+    }
 }
