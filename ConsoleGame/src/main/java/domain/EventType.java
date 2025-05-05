578bdc4bdb2b35a64621eeeaf5c8bbb1749549c2
en1y <marsentiy.cooo@gmail.com>
2025-05-05 15:56:00 +0200

    Implemented EventType

--- ConsoleGame/src/main/java/domain/EventType.java
+++ ConsoleGame/src/main/java/domain/EventType.java
@@ -1,9 +1,32 @@
 package domain;
 
+import org.fusesource.jansi.Ansi.Color;
+
 public enum EventType {
-    PLAY,
-    INFO,
-    INPUT,
-    ERROR,
-    WARNING
+    PLAY (Color.GREEN),
+    INFO (Color.CYAN),
+    INPUT (Color.GREEN),
+    ERROR (Color.RED),
+    WARNING (Color.YELLOW);
+
+    public static final int PREFIX_LEN = 20;
+
+    private final Color color;
+    private final String prefix;
+
+    EventType(Color color) {
+        this.color = color;
+        this.prefix =
+                String.format("[%s]%s",
+                        name(),
+                        " ".repeat(PREFIX_LEN - 2 - name().length()));
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
 }
