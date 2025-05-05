9a39b831a71175fd65484673bd2cb0d7be805f09
en1y <marsentiy.cooo@gmail.com>
2025-05-05 14:44:26 +0200

    In BriskulaLogic added:   Card.java   CardType.java In BriskulaLogic implemented CardType.java

---
+++ BriskulaLogic/src/main/java/cards/CardType.java
@@ -0,0 +1,44 @@
+package cards;
+
+public enum CardType {
+    SPADE (
+            "spade",
+            "špade"
+    ),
+    COPPE (
+            "coppe",
+            "kupe"
+    ),
+    DANARI (
+            "danari",
+            "dinari"
+    ),
+    BASTONI (
+            "bastoni",
+            "baštoni"
+    );
+
+    private String name;
+    private String croatianName;
+
+    CardType(String name, String croatianName) {
+        this.name = name;
+        this.croatianName = croatianName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCroatianName() {
+        return croatianName;
+    }
+
+    public void setCroatianName(String croatianName) {
+        this.croatianName = croatianName;
+    }
+}
