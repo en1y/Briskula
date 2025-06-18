3aff72573b171d3266dd42131f6a2b4c2345ca87
en1y <marsentiy.cooo@gmail.com>
2025-06-18 09:20:30 +0200

    implemented Card, CardFactory, CardType, CardValue

---
+++ cards/poker-cards/src/main/java/com/domain/CardType.java
@@ -0,0 +1,53 @@
+package com.domain;
+
+import java.util.Locale;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+/**
+ * This enum represents different <b>types</b> of cards.<br>
+ * Every card should have a name associated with it.<br>
+ * For implementing multiple languages, set the path to the config file with multiple language definitions.<br>
+ * The File should be named "CardTypeBundle_{lang}.properties" and placed in the resources directory.<br>
+ * Pattern: {CardType.name} = {lang.name}<br>
+ * Example: Spades = Spades<br>
+ */
+public enum CardType implements CardTypeInterface {
+
+    HEARTS ("Hearts"),
+    DIAMONDS ("Diamonds"),
+    CLUBS ("Clubs"),
+    SPADES ("Spades");
+
+    private static boolean useResourceBundle = false;
+    private static ResourceBundle resourceBundle;
+
+    private final String name;
+
+    CardType(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Use this method if you want to use the resource bundle for card type names.
+     * @param locale - with this locale the resource bundle will be loaded
+     */
+    public static void useResourceBundle(Locale locale) {
+        Objects.requireNonNull(locale);
+        useResourceBundle = true;
+
+        resourceBundle = ResourceBundle.getBundle("CardTypeBundle", locale);
+    }
+
+    /**
+     * When the resource bundle is not used, this method returns the name of the card type.
+     * When the resource bundle is used, it returns the name from the resource bundle.
+     * If the resource bundle does not contain the name, it returns the name of the card type.
+     */
+    public String getName() {
+        if (useResourceBundle && resourceBundle.containsKey(name)) {
+            return resourceBundle.getString(name);
+        }
+        return name;
+    }
+}
---
+++ cards/poker-cards/src/main/java/com/domain/CardValue.java
@@ -0,0 +1,102 @@
+package com.domain;
+
+import java.util.*;
+
+
+/**
+ * This enum represents different <b>values</b> of cards.<br>
+ * Values are automatically sorted in ascending order.
+ * For implementing multiple languages, set the path to the config file with multiple language definitions.<br>
+ * The File should be named "CardValueBundle_{lang}.properties" and placed in the resources directory.<br>
+ * Pattern: {CardValue.name}={lang.name}<br>
+ * Example: Two = Two<br>
+ */
+public enum CardValue implements CardValueInterface {
+    TWO ("Two", 2),
+    THREE ("Three", 3),
+    FOUR ("Four", 4),
+    FIVE ("Five", 5),
+    SIX ("Six", 6),
+    SEVEN ("Seven", 7),
+    EIGHT ("Eight", 8),
+    NINE ("Nine", 9),
+    TEN ("Ten", 10),
+    JACK ("Jack", 11),
+    Queen("Queen", 12),
+    KING ("King", 13),
+    ACE ("Ace", 14);
+
+    private static boolean useResourceBundle = false;
+    private static ResourceBundle resourceBundle;
+
+    private static Map<CardValue, Integer> cardValueMap = new HashMap<>();
+
+    public static final Comparator<CardValue> cardValueComparator = new Comparator<CardValue>() {
+        @Override
+        public int compare(CardValue o1, CardValue o2) {
+            var isValueEqual = Integer.compare(cardValueMap.get(o1), cardValueMap.get(o2));
+            var isNumberEqual = Integer.compare(o1.getNumber(), o2.getNumber());
+
+            if (isValueEqual != 0) {
+                return isValueEqual;
+            } else {
+                return isNumberEqual;
+            }
+        }
+    };
+
+    private final String name;
+    private final int number;
+
+    CardValue(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    static {
+        initializeCardValueMap();
+    }
+
+    private static void initializeCardValueMap() {
+        for (CardValue cardValue : CardValue.values()) {
+            cardValueMap.put(cardValue, cardValue.getNumber());
+        }
+    }
+
+    public static void setCardValueMap(Map<CardValue, Integer> cardValueMap) {
+        Objects.requireNonNull(cardValueMap, "Card value map cannot be null");
+
+        if (cardValueMap.keySet().containsAll(Arrays.asList(CardValue.values()))) {
+            CardValue.cardValueMap = cardValueMap;
+        } else {
+            throw new IllegalArgumentException("Card value map should contain all the card values");
+        }
+    }
+
+    /**
+     * Use this method if you want to use the resource bundle for card number names and values.
+     * @param locale - with this locale the resource bundle will be loaded
+     */
+    public static void useResourceBundle(Locale locale) {
+        Objects.requireNonNull(locale);
+        useResourceBundle = true;
+
+        resourceBundle = ResourceBundle.getBundle("CardValueBundle", locale);
+    }
+
+    /**
+     * When the resource bundle is not used, this method returns the name of the card number.
+     * When the resource bundle is used, it returns the name from the resource bundle.
+     * If the resource bundle does not contain the name, it returns the name of the card number.
+     */
+    public String getName() {
+        if (useResourceBundle && resourceBundle.containsKey(name)) {
+            return resourceBundle.getString(name);
+        }
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
