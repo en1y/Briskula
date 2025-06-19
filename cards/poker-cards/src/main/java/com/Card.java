64c5d319b5fc3a00ae84826007a6f69acc4ded42
en1y <marsentiy.cooo@gmail.com>
2025-06-19 10:44:13 +0200

    implemented ResourceBundles for english and croatian

--- cards/poker-cards/src/main/java/com/Card.java
+++ cards/poker-cards/src/main/java/com/Card.java
@@ -3,8 +3,24 @@ package com;
 import com.domain.CardType;
 import com.domain.CardValue;
 
+import java.util.Locale;
+
 public class Card extends AbstractCard {
     public Card(CardType type, CardValue value) {
         super(type, value);
+    }
+
+    public static void setLocale (Locale locale) {
+        CardType.useResourceBundle(locale);
+        CardValue.useResourceBundle(locale);
+    }
+
+    @Override
+    public String toString() {
+        return (
+                new StringBuffer()
+                        .append(getValue().getName())
+                        .append(getType().getName()).toString()
+        );
     }
 }
