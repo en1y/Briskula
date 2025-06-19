4095f7a809af31bd6804efae4a004ed8eebde530
en1y <marsentiy.cooo@gmail.com>
2025-06-19 12:16:08 +0200

    implemented AbstractPlayer and its classes

--- base-card-game/src/main/java/service/model/AbstractPlayer.java
+++ base-card-game/src/main/java/service/model/AbstractPlayer.java
@@ -1,15 +1,34 @@
 package service.model;
 
 import com.AbstractCard;
+import domain.Deck;
 import domain.Hand;
 import service.model.interfaces.PlayerInterface;
 
 public abstract class AbstractPlayer<Card extends AbstractCard> implements PlayerInterface<Card> {
 
     private Hand<Card> hand;
+    private final String name;
+
+    public AbstractPlayer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void getHandFromDeck(Deck<Card> deck, int cardsInHandNumber) {
+        hand = deck.createHand(cardsInHandNumber);
+    }
 
     @Override
     public void playCard(Card card) {
         hand.drawCard(card);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Hand<Card> getHand() {
+        return hand;
     }
 }
--- base-card-game/src/main/java/service/model/interfaces/PlayerInterface.java
+++ base-card-game/src/main/java/service/model/interfaces/PlayerInterface.java
@@ -1,9 +1,11 @@
 package service.model.interfaces;
 
 import com.AbstractCard;
+import domain.Deck;
 
 public interface PlayerInterface <Card extends AbstractCard>{
 
     void playCard(Card card);
+    void getHandFromDeck(Deck<Card> deck, int cardsInHandNumber);
 
 }
