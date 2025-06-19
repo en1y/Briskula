223212c17998910c3f402dda2539f3366d4d6d6b
en1y <marsentiy.cooo@gmail.com>
2025-06-19 10:51:55 +0200

    implemented Deck class in the base-card-game

---
+++ base-card-game/src/main/java/domain/exceptions/DeckException.java
@@ -0,0 +1,7 @@
+package domain.exceptions;
+
+public class DeckException extends RuntimeException {
+    public DeckException(String message, Object ... args) {
+        super(String.format(message, args));
+    }
+}
